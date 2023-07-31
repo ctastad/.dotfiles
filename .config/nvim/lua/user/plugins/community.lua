29ff9acc0b6b7f04c7d2102fb6ca3e41f7c5c897
ctastad <ctastad@gmail.com>
2023-07-31 10:12:10 -0400

    correct add of nvim config sans submodule

---
+++ .config/nvim/lua/user/plugins/community.lua
@@ -0,0 +1,9 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of imporing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  { import = "astrocommunity.colorscheme.nord-nvim" },
+  { import = "astrocommunity.completion.codeium-vim" },
+}
